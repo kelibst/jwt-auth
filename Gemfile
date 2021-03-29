f06307e272e384980a7e509f956292ad8d1f4b36
Keli Booster <kbooster17@gmail.com>
2021-03-29 04:20:53 +0000

    complete setup

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bcrypt', '~> 3.1.7'
 
 # Use Active Storage variant
+gem 'jwt'
 # gem 'image_processing', '~> 1.2'
 
 # Reduces boot times through caching; required in config/boot.rb
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-
+      resources :users 
+      post '/auth/login', to: 'authentication#login'
     end
   end
+ 
+  get '/*a', to: 'application#not_found'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
