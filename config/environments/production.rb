0ab7827343dd2d2c72d4841e49173da594422e20
Keli Booster <kbooster17@gmail.com>
2021-04-04 12:50:56 +0000

    update production.rb with mailer detials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,8 +55,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "jwt_auth_production"
 
-  config.action_mailer.perform_caching = false
-  https://jauth-keli.herokuapp.com/
+  config.action_mailer.perform_caching = false\
+  config.action_mailer.default_url_options = { host: 'https://jauth-keli.herokuapp.com' }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
    config.action_mailer.raise_delivery_errors = true
